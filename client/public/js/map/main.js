c15043149cbaf64c25f2e6dbc2defc6e63d82bc3
Alessandro Morelato <alessandro.morelato@studenti.unitn.it>
2025-05-31 11:44:21 +0200

    Search bar per feature/city

--- client/public/js/map/main.js
+++ client/public/js/map/main.js
@@ -1,27 +1,38 @@
 //Setta la mappa
-//const { fetchDataField, convertToWGS84 } = window.utils; // Dobbiamo caricare i  dati sulla città
+const nomeCittaInput = document.getElementById('search-city');
+const map = L.map('leaflet-map');
 
-//  Per ora default Trento
-const nomeCitta = "Trento";     //ADD search bar in index.ejs
+async function loadCitta(map, nomeCitta) {
 
-fetchDataField('citta', 'nome', nomeCitta).then(citta => {
-    citta = citta[0];
-    const map = L.map('leaflet-map', {
-        center: convertToWGS84(citta.pos.x, citta.pos.y),   //Pos Trento,
-        zoom: parseInt(citta.zoom.min)+2,                             //Zoom iniziale, si vede tutta la città
-        maxBounds: [//non fa allontanare dal centro
+    fetchDataField('citta', 'nome', nomeCitta).then(citta => {
+        citta = citta[0];
+        if (!citta) {
+            alert("La città non è presente nei DataBase di StreetWise");
+            return;
+        }
+
+        const center = convertToWGS84(citta.pos.x, citta.pos.y);
+        const bounds = L.latLngBounds(
             convertToWGS84(citta.bounds.left, citta.bounds.down),
             convertToWGS84(citta.bounds.right, citta.bounds.up)
-        ],
-        maxBoundsViscosity: 0.7 //fa effetto elastico sugli angoli
-    }) //pos
+        )
 
-    map.setMinZoom(parseInt(citta.zoom.min));
-    map.setMaxZoom(parseInt(citta.zoom.max));
+        map.setView(center, parseInt(citta.zoom.min)+2);
+        map.setMinZoom(parseInt(citta.zoom.min));
+        map.setMaxZoom(parseInt(citta.zoom.max));
+        map.setMaxBounds(bounds, maxBoundsViscosity=0.9);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; OpenStreetMap contributors'
-    }).addTo(map);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; OpenStreetMap contributors'
+        }).addTo(map);
 
-    loadAllMapData(map, citta.id);
+        loadAllMapData(map, citta.id);
+    });
+
+};
+
+loadCitta(map, nomeCittaInput.value);
+
+nomeCittaInput.addEventListener("change", () => {
+    loadCitta(map, nomeCittaInput.value)
 });
